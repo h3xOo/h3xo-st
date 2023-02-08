0081b74cf5ba52996f4e9f421c822e6109aa2f60
Stanisław Wiśniewski <s.wisniewski1101@gmail.com>
2023-02-08 14:30:45 +0100

    Headers: clang-format

--- hb.h
+++ hb.h
@@ -1,14 +1,15 @@
 #ifndef HB_H__
 #define HB_H__
 
-#include <stddef.h>
 #include <X11/Xft/Xft.h>
-#include <hb.h>
 #include <hb-ft.h>
+#include <hb.h>
+#include <stddef.h>
 
 #include "st.h"
 
 void hbunloadfonts();
-void hbtransform(XftGlyphFontSpec *specs, const Glyph *glyphs, size_t len, int x, int y);
+void hbtransform(XftGlyphFontSpec *specs, const Glyph *glyphs, size_t len,
+                 int x, int y);
 
 #endif /* HB_H__ */
--- x.h
+++ x.h
@@ -1,54 +1,50 @@
 #ifndef X_H__
 #define X_H__
 
-#include <sys/types.h>
-#include <X11/keysym.h>
+#include "st.h"
 #include <X11/X.h>
 #include <X11/Xft/Xft.h>
-#include "st.h"
+#include <X11/keysym.h>
+#include <sys/types.h>
 
 /* types used in config.h */
 typedef struct {
-	uint mod;
-	KeySym keysym;
-	void (*func)(const Arg *);
-	const Arg arg;
+    uint mod;
+    KeySym keysym;
+    void (*func)(const Arg *);
+    const Arg arg;
 } Shortcut;
 
 typedef struct {
-	uint mod;
-	uint button;
-	void (*func)(const Arg *);
-	const Arg arg;
-	uint  release;
+    uint mod;
+    uint button;
+    void (*func)(const Arg *);
+    const Arg arg;
+    uint release;
 } MouseShortcut;
 
 typedef struct {
-	KeySym k;
-	uint mask;
-	char *s;
-	/* three-valued logic variables: 0 indifferent, 1 on, -1 off */
-	signed char appkey;    /* application keypad */
-	signed char appcursor; /* application cursor */
+    KeySym k;
+    uint mask;
+    char *s;
+    /* three-valued logic variables: 0 indifferent, 1 on, -1 off */
+    signed char appkey;    /* application keypad */
+    signed char appcursor; /* application cursor */
 } Key;
 
 /* Xresources preferences */
-enum resource_type {
-	STRING = 0,
-	INTEGER = 1,
-	FLOAT = 2
-};
+enum resource_type { STRING = 0, INTEGER = 1, FLOAT = 2 };
 
 typedef struct {
-	char *name;
-	enum resource_type type;
-	void *dst;
+    char *name;
+    enum resource_type type;
+    void *dst;
 } ResourcePref;
 
 /* X modifiers */
-#define XK_ANY_MOD    UINT_MAX
-#define XK_NO_MOD     0
-#define XK_SWITCH_MOD (1<<13|1<<14)
+#define XK_ANY_MOD UINT_MAX
+#define XK_NO_MOD 0
+#define XK_SWITCH_MOD (1 << 13 | 1 << 14)
 
 /* function definitions used in config.h */
 static void clipcopy(const Arg *);
@@ -64,14 +60,14 @@ static void ttysend(const Arg *);
 /* config.h for applying patches and the configuration. */
 
 /* XEMBED messages */
-#define XEMBED_FOCUS_IN  4
+#define XEMBED_FOCUS_IN 4
 #define XEMBED_FOCUS_OUT 5
 
 /* macros */
-#define IS_SET(flag)		((win.mode & (flag)) != 0)
-#define TRUERED(x)		(((x) & 0xff0000) >> 8)
-#define TRUEGREEN(x)		(((x) & 0xff00))
-#define TRUEBLUE(x)		(((x) & 0xff) << 8)
+#define IS_SET(flag) ((win.mode & (flag)) != 0)
+#define TRUERED(x) (((x)&0xff0000) >> 8)
+#define TRUEGREEN(x) (((x)&0xff00))
+#define TRUEBLUE(x) (((x)&0xff) << 8)
 
 typedef XftDraw *Draw;
 typedef XftColor Color;
@@ -79,70 +75,71 @@ typedef XftGlyphFontSpec GlyphFontSpec;
 
 /* Purely graphic info */
 typedef struct {
-	int tw, th; /* tty width and height */
-	int w, h; /* window width and height */
-	int ch; /* char height */
-	int cw; /* char width  */
-	int mode; /* window state/mode flags */
-	int cursor; /* cursor style */
+    int tw, th; /* tty width and height */
+    int w, h;   /* window width and height */
+    int ch;     /* char height */
+    int cw;     /* char width  */
+    int mode;   /* window state/mode flags */
+    int cursor; /* cursor style */
 } TermWindow;
 
 typedef struct {
-	Display *dpy;
-	Colormap cmap;
-	Window win;
-	Drawable buf;
-	GlyphFontSpec *specbuf; /* font spec buffer used for rendering */
-	Atom xembed, wmdeletewin, netwmname, netwmiconname, netwmpid;
-	struct {
-		XIM xim;
-		XIC xic;
-		XPoint spot;
-		XVaNestedList spotlist;
-	} ime;
-	Draw draw;
-	Visual *vis;
-	XSetWindowAttributes attrs;
-	int scr;
-	int isfixed; /* is fixed geometry? */
-	int depth; /* bit depth */
-	int l, t; /* left and top offset */
-	int gm; /* geometry mask */
+    Display *dpy;
+    Colormap cmap;
+    Window win;
+    Drawable buf;
+    GlyphFontSpec *specbuf; /* font spec buffer used for rendering */
+    Atom xembed, wmdeletewin, netwmname, netwmiconname, netwmpid;
+    struct {
+        XIM xim;
+        XIC xic;
+        XPoint spot;
+        XVaNestedList spotlist;
+    } ime;
+    Draw draw;
+    Visual *vis;
+    XSetWindowAttributes attrs;
+    int scr;
+    int isfixed; /* is fixed geometry? */
+    int depth;   /* bit depth */
+    int l, t;    /* left and top offset */
+    int gm;      /* geometry mask */
 } XWindow;
 
 typedef struct {
-	Atom xtarget;
-	char *primary, *clipboard;
-	struct timespec tclick1;
-	struct timespec tclick2;
+    Atom xtarget;
+    char *primary, *clipboard;
+    struct timespec tclick1;
+    struct timespec tclick2;
 } XSelection;
 
 /* Font structure */
 #define Font Font_
 typedef struct {
-	int height;
-	int width;
-	int ascent;
-	int descent;
-	int badslant;
-	int badweight;
-	short lbearing;
-	short rbearing;
-	XftFont *match;
-	FcFontSet *set;
-	FcPattern *pattern;
+    int height;
+    int width;
+    int ascent;
+    int descent;
+    int badslant;
+    int badweight;
+    short lbearing;
+    short rbearing;
+    XftFont *match;
+    FcFontSet *set;
+    FcPattern *pattern;
 } Font;
 
 /* Drawing Context */
 typedef struct {
-	Color *col;
-	size_t collen;
-	Font font, bfont, ifont, ibfont;
-	GC gc;
+    Color *col;
+    size_t collen;
+    Font font, bfont, ifont, ibfont;
+    GC gc;
 } DC;
 
 static inline ushort sixd_to_16bit(int);
-static int xmakeglyphfontspecs(XftGlyphFontSpec *, const Glyph *, int, int, int);
+static int xmakeglyphfontspecs(XftGlyphFontSpec *, const Glyph *, int, int,
+                               int);
 static void xdrawglyphfontspecs(const XftGlyphFontSpec *, Glyph, int, int, int);
 static void xdrawglyph(Glyph, int, int);
 static void xclear(int, int, int, int);
