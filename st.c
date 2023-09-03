7751c70d7be966e4a339a19c4ad5ca4c3f1a3b39
Stanisław Wiśniewski <s.wisniewski1101@gmail.com>
2023-09-03 21:46:54 +0200

    Fix memory leak in xstrdup()

--- st.c
+++ st.c
@@ -276,8 +276,6 @@ void* xrealloc(void* p, size_t len) {
 }
 
 char* xstrdup(const char* s) {
-    if ((s = strdup(s)) == NULL)
-        die("strdup: %s\n", strerror(errno));
     char* p;
 
     if ((p = strdup(s)) == NULL)
