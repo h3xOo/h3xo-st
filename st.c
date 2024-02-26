b23aa9e56c1d7ff024f569c45ea159d10e6dbf56
Stanisław Wiśniewski <s.wisniewski1101@gmail.com>
2024-02-26 18:38:55 +0100

    Upstream patches

--- st.c
+++ st.c
@@ -96,8 +96,8 @@ enum escape_state {
 
 typedef struct {
         Glyph attr; /* current char attributes */
-        int x;
-        int y;
+        int x;      /* terminal column */
+        int y;      /* terminal row */
         char state;
 } TCursor;
 
@@ -1733,6 +1733,8 @@ unknown:
                 }
                 break;
         case 'S': /* SU -- Scroll <n> line up */
+                if (csiescseq.priv)
+                        break;
                 DEFAULT(csiescseq.arg[0], 1);
                 tscrollup(term.top, csiescseq.arg[0], 0);
                 break;
@@ -2243,12 +2245,16 @@ void tstrsequence(uchar c)
 
 void tcontrolcode(uchar ascii)
 {
+        size_t i;
+
         switch (ascii) {
         case '\t': /* HT */
                 tputtab(1);
                 return;
         case '\b': /* BS */
-                tmoveto(term.c.x - 1, term.c.y);
+                for (i = 1; term.c.x && term.line[term.c.y][term.c.x - i].u == 0; ++i)
+                        ;
+                tmoveato(term.c.x - i, term.c.y);
                 return;
         case '\r': /* CR */
                 tmoveto(0, term.c.y);
